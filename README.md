c8131e5ee645b26c4acf2469077a48022f8ba600
sumitjain <sumi12june@gmail.com>
2021-02-25 22:50:40 -0600

    JWT implementation

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 - sudo pip3 install virtualenv
 - virtualenv venv --python=python3
 - source venv/bin/activate
-- source venv/bin/activate
 - python --version (Should show latest python version)
 - deactivate (To come out virtual env) 
 
--- app.py
+++ app.py
@@ -1,15 +1,53 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = 'sjain'
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity)  # /auth
+items = []
 
-class Student(Resource):
+
+class Item(Resource):
+    @jwt_required()
     def get(self, name):
-        return {'student': name}
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': item}, 200 if item else 404
+
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'message': "An Item with name '{}' already exist".format(name)}, 400
+
+        data = request.get_json()
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {"message": name + " item is deleted"}
+
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item, 201
 
 
-api.add_resource(Student, '/student/<string:name>')  # 127.0.0.1:5000/student/sumit
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
 
-app.run(port=5000)
+
+api.add_resource(Item, '/item/<string:name>')  # 127.0.0.1:5000/student/sumit
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
